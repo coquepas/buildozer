b3f0b8fa2b446619daedb669d08980a2e9d8c515
coquepas <coquepas@gmail.com>
2024-09-14 00:17:25 +0200

    ok webview

--- pythonforandroid/bootstraps/webview/build/src/main/java/org/kivy/android/PythonActivity.java
+++ pythonforandroid/bootstraps/webview/build/src/main/java/org/kivy/android/PythonActivity.java
@@ -161,7 +161,7 @@ public class PythonActivity extends Activity {
             //dealing with the bouncing effect
             mWebView.setOverScrollMode(2); 
             //hide the f** scrollbar
-            webView.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
+            mwebView.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
             //resto de cosas predefinidas
             mWebView.getSettings().setJavaScriptEnabled(true);
             mWebView.getSettings().setDomStorageEnabled(true);
